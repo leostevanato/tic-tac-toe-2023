0574ca1ebacecb1db9bda6946c202967b053b573
leostevanato <47180899+leostevanato@users.noreply.github.com>
2023-09-22 16:32:47 -0300

    Testing for dynamically create the board

--- src/App.js
+++ src/App.js
@@ -1,3 +1,43 @@
-export default function Square() {
-  return <button className="square">X</button>;
+export default function Board() {
+  function Square({ value }) {
+    return <button className="square">{value}</button>;
+  }
+
+  let rows = 3;
+  let squares_per_row = 3;
+  let total_squares = 9;
+  let count_squares = 0;
+  let rows_elements = [];
+  let squares_elements = [];
+
+  const group_rows_squares = Array.from(
+    { length: total_squares }, (_, i) => i + 1
+  )
+    .reduce((groups, currentValue) => {
+      const arr = groups[groups.length - 1];
+      arr.push(currentValue);
+      if (arr.length === 3) groups.push([]);
+      return groups;
+    }, [[]])
+    .filter((chunk) => chunk.length);
+  
+  // return (
+  //   <>
+  //     <div className="board-row">
+  //       <Square key={i++} value={i++} />
+  //       <Square key={i++} value={i++} />
+  //       <Square key={i++} value={i++} />
+  //     </div>
+  //     <div className="board-row">
+  //       <Square key={i++} value={i++} />
+  //       <Square key={i++} value={i++} />
+  //       <Square key={i++} value={i++} />
+  //     </div>
+  //     <div className="board-row">
+  //       <Square key={i++} value={i++} />
+  //       <Square key={i++} value={i++} />
+  //       <Square key={i++} value={i++} />
+  //     </div>
+  //   </>
+  // );
 }
