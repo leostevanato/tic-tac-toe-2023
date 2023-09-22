a1aa0b96b2716e609f67fa89cdb25eefff8d5fe9
leostevanato <47180899+leostevanato@users.noreply.github.com>
2023-09-22 18:50:26 -0300

    Create Board class and Row component

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ export default function Board() {
     return <button className="square">{value}</button>;
   }
 
+  function Row({ squares }) {
+    return <div className="board-row">
+      {squares.map((square, sindex) => <Square key={sindex} value={square} />)}
+    </div>;
+  }
+
   let squares_per_row = 3;
   let total_squares = 9;
 
-  return Array.from(
-    { length: total_squares }, (_, i) => i + 1
-  )
-    .reduce((rows, currentValue) => {
-      const arrTemp = rows[rows.length - 1];
-      arrTemp.push(currentValue);
-      if (arrTemp.length === squares_per_row) rows.push([]);
-      return rows;
-    }, [[]])
-    .filter((chunk) => chunk.length) // Clean up last empty array
-    .map((row, rindex) => (
-      <div key={rindex} className="board-row">
-        {row.map((square, sindex) => <Square key={sindex} value={square} />)}
-      </div>
-    ));
+  return (
+    <div className="board">
+      {
+        Array.from({ length: total_squares }, (_, i) => i + 1)
+          .reduce((rows, currentValue) => {
+            const arrTemp = rows[rows.length - 1];
+            arrTemp.push(currentValue);
+            if (arrTemp.length === squares_per_row) rows.push([]);
+            return rows;
+          }, [[]])
+          .filter((chunk) => chunk.length) // Clean up last empty array
+          .map((row, rindex) => <Row key={rindex} squares={row} />)
+      }
+    </div>
+  );
 }
