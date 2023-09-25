e47956c3ec13a01dc41e6e5a5ad10da40e4cea5f
leostevanato <47180899+leostevanato@users.noreply.github.com>
2023-09-25 11:17:00 -0300

    Update App clicks and board squares state handling

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
+import { useState } from "react";
+
 export default function Board() {
-  function Square({ value }) {
-    return <button className="square">{value}</button>;
+  function Square({ value, onSquareClick }) {
+    return <button className="square" onClick={onSquareClick}>{value}</button>;
   }
 
-  function Row({ squares }) {
+  function Row({ firstIndex, childSquares, handleSquareClick }) {
     return <div className="board-row">
-      {squares.map((square, sindex) => <Square key={sindex} value={square} />)}
+      {childSquares.map((value, sindex) => <Square key={sindex} value={value} onSquareClick={() => handleSquareClick(firstIndex + sindex)} />)}
     </div>;
+  }
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
   }
 
   let squares_per_row = 3;
   let total_squares = 9;
+  const [squares, setSquares] = useState(Array(total_squares).fill(null));
 
   return (
     <div className="board">
       {
-        Array.from({ length: total_squares }, (_, i) => i + 1)
-          .reduce((rows, currentValue) => {
+        squares.reduce((rows, currentValue) => {
             const arrTemp = rows[rows.length - 1];
             arrTemp.push(currentValue);
             if (arrTemp.length === squares_per_row) rows.push([]);
+          
             return rows;
           }, [[]])
           .filter((chunk) => chunk.length) // Clean up last empty array
-          .map((row, rindex) => <Row key={rindex} squares={row} />)
-      }
+          .map((row, rindex) => <Row key={rindex} firstIndex={squares_per_row * rindex} childSquares={row} handleSquareClick={handleClick} />)
+        }
     </div>
   );
 }
